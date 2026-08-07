470979809bff3b8c358d86b530d03d40b3453617
Prasanta Karan <prasanta.adgion@gmail.com>
2026-08-07 18:43:44 +0530

    feat: Add camera icon to AppIcons and implement profile photo upload functionality

--- lib/core/theme/app_icons.dart
+++ lib/core/theme/app_icons.dart
@@ -72,6 +72,7 @@ abstract final class AppIcons {
   static const IconData inactive = Icons.pause_circle_outline_rounded;
 
   // Profile
+  static const IconData camera = Icons.photo_camera_rounded;
   static const IconData gender = Icons.wc_rounded;
   static const IconData birthday = Icons.cake_outlined;
   static const IconData address = Icons.location_on_outlined;
